3c02255b7a36636c5cb7eacbf619e9a3963db5fd
Russell Cloran <rcloran@gmail.com>
2013-05-20 08:42:12 -0700

    First pass, mostly a copy of nmilford/stormLauncher

---
+++ missile.py
@@ -0,0 +1,44 @@
+# Copyright 2012 Nathan Milford
+# Copyright 2013 Russell Cloran
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import usb.core
+
+
+class LaunchControl(object):
+    def __init__(self):
+        self.dev = usb.core.find(idVendor=0x2123, idProduct=0x1010)
+        if self.dev is None:
+            raise ValueError('Launcher not found.')
+        if self.dev.is_kernel_driver_active(0) is True:
+            self.dev.detach_kernel_driver(0)
+        self.dev.set_configuration()
+
+    def turret_up(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def turret_down(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def turret_left(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def turret_right(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def turret_stop(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def fire(self):
+        self.dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
